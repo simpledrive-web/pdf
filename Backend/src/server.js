ef382e883ebb3329da21ba11f3a12dd96d33f25b
Isaias Peixoto <peixotoisaias68@gmail.com>
2026-05-20 12:54:20 -0300

    fix: correção server

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -25,7 +25,7 @@ app.use("/api/chats", chatRoutes);
 
 app.use("/api/messages", messageRoutes);
 
-app.use("/api/chat", chatAIRoutes);
+app.use("/api", chatAIRoutes);
 
 app.use("/api", imageRoutes);
 
